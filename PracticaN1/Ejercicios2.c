ae4dcb5065b135ef7da029d3166aa1e28416f291
Milagros Jara <milagrosjarag20@gmail.com>
2026-04-28 21:35:38 -0300

    Ejercicios2.c

---
+++ PracticaN1/Ejercicios2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+int ecu, valor=0;
+ecu=valor*valor+1;
+printf ("Si el valor=%d, el resultado es =%d\n", valor, ecu);
+valor=2;
+ecu=valor*valor+1;
+printf ("Si el valor=%d, el resultado es =%d\n", valor, ecu);
+return 0;
+}
