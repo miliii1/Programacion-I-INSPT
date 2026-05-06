bdaf1845c69831a621f5eb97d598914a7609e91f
Milagros Jara <milagrosjarag20@gmail.com>
2026-05-06 11:35:26 -0300

    Add user menu with switch statement
    Implement a switch statement for user menu options.

---
+++ PracticaN1/switch.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+intmain()
+{
+    int valor; //Variable de control del switch
+    printf("Ingrese un numero tenieno en cuenta el siguiente menu\n");
+    printf("\t1- Jugar\n");
+    printf("\t2- Puntajes\n");
+    printf("\t2- Configurar\n");
+    printf("\t3- Salir\n ");
+    scanf("%d", &valor);
+    switch(valor)
+    {
+        case 1:
+        printf ("Usted esta jugando");
+        break; //opcional
+        case 2:
+        printf("Ver las puntuaciones");
+        break; //opcional
+        case 3:
+        printf 
+
+
+
+    }
