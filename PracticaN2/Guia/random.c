f2c61a2e4d33ba760b1abfbc7b99f7d4f05fc396
Milagros Jara <milagrosjarag20@gmail.com>
2026-05-27 10:57:29 -0300

    ejercicio11 guia 3

--- PracticaN2/Guia/random.c
+++ PracticaN2/Guia/random.c
@@ -22,3 +22,14 @@ int main()
     printf("Se encuentran %d numeros capicua", cont);
     return 0;
 }
+// ejercicio 11 de la guia 3
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int i;
+    for(i = 1; i <= 50; i++) {
+        if(i % 2 == 0) printf("%d\n", i);
+    }
+    return 0;
+}
