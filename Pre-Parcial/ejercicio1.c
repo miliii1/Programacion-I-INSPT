894351d34fe58f517cd6241341a0987cf6f6f66e
Milagros Jara <milagrosjarag20@gmail.com>
2026-07-01 11:14:18 -0300

    SIN TERMINAR

---
+++ Pre-Parcial/ejercicio1.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void maximo(int*, int);
+
+int numCaja, numSeccion, importe, cantProdu, max;
+int descuento, descuentoTotal = 0, contComprobante = 0, recaudacionTotal = 0;
+char operar;
+
+int main()
+{
+
+    do
+    {
+        printf("Ingrese NRO. de CAJA(1 o 2): ");
+        scanf("%d", &numCaja);
+        printf("Ingrese NRO. de SECCION(1 a 3): ");
+        scanf("%d", &numSeccion);
+        printf("Ingrese IMPORTE DE LA VENTA: ");
+        scanf("%d", &importe);
+        printf("Ingrese CANTIDAD DE PRODUCTOS COMPRADOS: ");
+        scanf("%d", &cantProdu);
+
+
+        if (importe >= 5000)
+        {
+            descuento = importe - ((importe / 100) * 10);
+            printf("El importe total es: %d\n", descuento);
+        }
+        
+        printf("¿Desea seguir operando?(S o N): ");
+        scanf(" %c", &operar);
+
+        contComprobante++;
+        if(descuento >= 5000) {
+            recaudacionTotal+=descuento;
+        } else {
+            recaudacionTotal+=importe;
+        }
+
+
+    } while (operar != 'N' && operar != 'n');
+
+    
+    printf("Cantidad de comprobantes emitidos en cada caja: %d\n", contComprobante);
+    printf("Recaudacion total: $%d", recaudacionTotal);
+    return 0;
+}
+
+void maximo(int *importeMax, int cajaMax) {
+    int importeMax = importe;
+    
+}
